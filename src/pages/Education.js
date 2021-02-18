654cf8da362a85aa2ae32b7f95eb80cb50862005
Wylhem dorville <62334126+Wylhem@users.noreply.github.com>
2021-02-18 16:03:43 +0100

    Adding education pages

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import ButtonScroll from "../components/buttonsScroll/ButtonScroll";
+import Navbar from "../components/Navbar/Navbar";
+import {motion} from "framer-motion";
 
 const Education = () => {
     return (
         <div className="container">
+            <Navbar />
+            <div className="educ-containt">
+                <div className="grid-left">
+                    <h1>formation</h1>
+                    <ul>
+                        <li><p>2019-2021 : Université Paris Saclay  : DUT informatique</p></li>
+                        <li><p>2016-2019 : Lycée René Cassin</p> </li>
+                    </ul>
+                </div>
+                <div className="grid-right">
+                    <motion.span
+                        className="circle-1"
+                        drag OnDragEnd
+                        dragConstraints={{left:-150 , right:10 , top : -5 , bottom : 50}}
+                        whileTap={{scale : 0.5}}
+                    >
+                    </motion.span>
+                    <motion.span
+                        className="circle-2"
+                        drag OnDragEnd
+                        dragConstraints={{left:-150 , right:10 , top : -5 , bottom : 50}}
+                        whileTap={{scale : 0.5}}
+                    >
+                    </motion.span>
+                    <motion.span
+                        className="circle-3"
+                        drag OnDragEnd
+                        dragConstraints={{left:-80 , right:15 , top : -5 , bottom : 2}}
+                        whileTap={{scale : 0.5}}
+                    >
+                    </motion.span>
+                    <motion.span
+                        className="circle-4"
+                        drag OnDragEnd
+                        dragConstraints={{left:50 , right:5 , top : -5 , bottom : 10}}
+                        whileTap={{scale : 0.8}}
+                    >
+                    </motion.span>
+
+                </div>
+            </div>
             <ButtonScroll left={"/"} right={"/portfolio"}/>
-        </div>
+        </div >
     );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar/Navbar";
 import SocialNetwork from "../components/SocialNetwork/SocialNetwork";
 import DynamicText from "../components/DynamicText/DynamicText";
 import ButtonScroll from "../components/buttonsScroll/ButtonScroll";
+import {motion} from "framer-motion";
 
 const Home = () => {
     return (
@@ -12,8 +13,16 @@ const Home = () => {
                 <SocialNetwork/>
                 <div className="home-main">
                     <div className="home-content">
-                        <h1>Wylhem Dorville</h1>
-                        <h2><DynamicText/></h2>
+                        <motion.h1
+                            drag OnDragEnd
+                            dragConstraints={{left:-250 , right:250 , top : -250 , bottom : 250}}
+                        >
+                            Wylhem Dorville
+                        </motion.h1>
+                        <motion.h2
+                            drag OnDragEnd
+                            dragConstraints={{left:-250 , right:250 , top : -250 , bottom : 250}}
+                        ><DynamicText/></motion.h2>
                     </div>
                 </div>
                 <ButtonScroll right={"/education"} />
